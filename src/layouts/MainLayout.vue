6f288dabff1a0df833f5fdc0d1972cf518ad0b9d
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-10-08 17:52:13 +0100

    initial bootstrap and barebones layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          Rogerio's Portfolio
+          <q-badge color="primary" align="top">v2.0</q-badge>
+        </q-toolbar-title>
+        
+
+        <q-avatar size="lg">
+          <img src="../statics/images/rogerio_avatar.jpeg" alt="avatar">
+        </q-avatar>
+
+
+      </q-toolbar>
+      <q-banner class="bg-dark text-white" inline-actions>
+        Welcome to my online portfolio / CV. Check out some featured projects here or click the links to see full portfolio, and about / contact pages.
+        <template v-slot:action>
+          <q-btn-group rounded c>
+            <q-btn rounded label="Portfolio" icon="folder" text-color="light-blue-11" to="/portfolio" />
+            <q-btn rounded label="About" icon="info" text-color="light-blue-11" to="/about" />
+          </q-btn-group>
+        </template>
+      </q-banner>
+    </q-header>
+
+
+    <q-page-container>
+      
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+
+    return {
+    }
+  }
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex bg-light-blue-1">
+    <h5 class="text-h5 text-weight-light text-uppercase text-blue-grey">Featured Projects</h5>
+    
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
