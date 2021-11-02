0f3a5bab1b069f3f133de32c0a266d25853fc856
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-11-02 14:26:06 +0000

    portfolio item details markup done and added on first item

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,58 +1,146 @@
 <template>
   <q-page class="flex column bg-light-blue-1">
+
     <q-layout view="hHh Lpr lff">
       
       <q-page-container>
+
         <q-drawer
-        v-model="drawer"
-        show-if-above
-        :width="300"
-        :breakpoint="500"
-        bordered
-        class="bg-blue text-white q-pt-xl q-mt-xl"
-      >
-        <q-scroll-area class="fit q-mt-lg">
-          <q-list class="q-pt-md">
-            <template v-for="project in projects" :key="project.id">
-              <q-item clickable v-ripple @click="selectedProject = project.id">
-                <q-item-section>
-                  <q-item class="text-subtitle1 text-weight-medium">
-                    {{ project.name }}
-                  </q-item>
-                  <q-item>
-                    <q-img :src="project.image" height="200px"></q-img>
-                  </q-item>
-                </q-item-section>
-              </q-item>
-            </template>
-          </q-list>
-        </q-scroll-area>
-      </q-drawer>
+          v-model="drawer"
+          show-if-above
+          :width="300"
+          :breakpoint="500"
+          bordered
+          class="bg-blue text-white q-pt-xl q-mt-xl"
+        >
+
+          <q-scroll-area class="fit q-mt-lg">
+
+            <q-list class="q-pt-md">
+
+              <template v-for="project in projects" :key="project.id">
+
+                <q-item clickable v-ripple @click="selectedProject = project.id">
+
+                  <q-item-section>
+
+                    <q-item class="text-subtitle1 text-weight-medium">
+                      {{ project.name }}
+                    </q-item>
+
+                    <q-item>
+                      <q-img :src="project.image" height="200px"></q-img>
+                    </q-item>
+
+                  </q-item-section>
+
+                </q-item>
+
+              </template>
+
+            </q-list>
+
+          </q-scroll-area>
+
+        </q-drawer>
+
         <q-page padding>
+
           <h4 class="q-pl-md text-h4 text-weight-light text-uppercase text-blue-grey ">Portfolio</h4>
+
           <q-separator />
+
           <q-layout view="hHh lpR fFf" class="q-mt-lg">
+
             <h4 class="text-h4">
               {{currentProject.name}}
             </h4>
+
             <p class="text-subtitle2">
               Type: {{currentProject.type}}
             </p>
+
             <div>
               <q-badge  color="primary" v-for="tech in currentProject.technologies" :key="tech" :label="tech" class="q-mr-sm" />
             </div>
+
             <p class="text-subtitle2">
               {{currentProject.shortDesc}}
             </p>
+
             <q-img :src="currentProject.image" width="800px"></q-img>
+
             <p class="text-subtitle1">
               {{currentProject.longDesc}}
             </p>
-        </q-layout>
+
+            <div v-if="currentProject.showDetails">
+
+              <h3>
+                Project Details
+              </h3>
+
+              <p>
+                {{ currentProject.details.head }}
+              </p>
+
+              <div>
+
+                <img
+                  v-for="(image, i) in currentProject.details.images1"
+                  :key="i"
+                  :src="image"
+                  alt="details from this project"
+                />
+
+              </div>
+
+              <div v-if="currentProject.details.body">
+
+                <p>{{ currentProject.details.body }}</p>
+
+                <div>
+
+                  <img
+                    v-for="(image, i) in currentProject.details.images2"
+                    :key="i"
+                    :src="image"
+                    alt="details from this project"
+                  />
+
+                </div>
+
+              </div>
+
+              <div v-if="currentProject.details.footer">
+
+                <p>{{ currentProject.details.footer }}</p>
+
+                <div>
+
+                  <img
+                    v-for="(image, i) in currentProject.details.images3"
+                    :key="i"
+                    :src="image"
+                    alt="details from this project"
+                  />
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </q-layout>
+
         </q-page>
+
       </q-page-container>
+
     </q-layout>
+
   </q-page>
+  
 </template>
 
 <script>
--- src/store/projects/state.js
+++ src/store/projects/state.js
@@ -12,7 +12,16 @@ export default function () {
         live: "https://codepen.io/rogerio-romao/full/RwPaaRR",
         longDesc:
           "Unit Conversion Program, does distances, weights and temperatures. Sleek flat style design and attention to user experience were my focus, as well as functional and well structured and documented code. Built using JQuery.",
-        details: true,
+        showDetails: true,
+        details: {
+          head: "This was the first app i created from scratch since coming back to coding, without following any tutorials. I had the idea and started by thinking about the interface and selecting a pleasing color scheme and coming up with the layout and the name. There is an animation on the title/logo h1 that conveys the idea of conversion. There are svg icons for the different types of conversion with custom tooltips to indicate what they do. The select boxes are also super customized and look great in my opinion. CSS was done from scratch, no CSS frameworks. I coded it using Sass and variables for easy modification and flexibility. The app works well at any screen size via media queries. I used jQuery for the DOM stuff and the javascript is all commented out to explain what the code does. The rest was just researching the conversion rates and implementing the inputs and a lot of conversion functions.",
+          images1: [
+            require("../../assets/converse-details1.png"),
+            require("../../assets/converse-details2.png"),
+            require("../../assets/converse-details3.png"),
+            require("../../assets/converse-details4.png"),
+          ],
+        },
       },
       {
         id: 2,
