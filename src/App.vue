697ce61c13cc8191f9771d1e2e8b420dcde30528
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2022-02-08 19:06:35 +0000

    favicon metadata in app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-    <router-view v-slot="{Component}">
-      <transition
-        appear
-        enter-active-class="animated fadeIn"
-        leave-active-class="animated fadeOut"
-      >
-        <component :is="Component" />
-      </transition>
-    </router-view>
+  <router-view v-slot="{ Component }">
+    <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 <script>
 import { defineComponent, Component } from 'vue';
-
+import { useMeta } from 'quasar'
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    const metadata = useMeta({
+      link: {
+        rel: 'icon',
+        type: 'image/ico',
+        href: 'favicon.ico',
+      },
+    })
+    return {
+      metadata
+    }
+  }
 })
 </script>
