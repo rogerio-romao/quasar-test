85ded72b3daffeda15b4ed2050ebc6d75e1090d6
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2024-05-27 14:38:32 +0100

    feat: cv

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -9,22 +9,53 @@
       <q-badge color="primary">&copy; 2022</q-badge>
 
       <q-avatar size="lg">
-        <img src="../statics/images/rogerio_avatar.jpeg" alt="avatar" class="avatar" />
+        <img
+          src="../statics/images/rogerio_avatar.jpeg"
+          alt="avatar"
+          class="avatar"
+        />
       </q-avatar>
     </q-toolbar>
     <q-banner class="bg-dark text-white" inline-actions>
       Welcome to my online portfolio / CV / blog.
       <template v-slot:action>
-        <q-tabs inline-label dense shrink class="text-cyan-13" active-color="light-green">
-          <q-route-tab icon="home" to="/featured" :label="$q.screen.gt.sm ? 'Featured' : ''" exact />
+        <q-tabs
+          inline-label
+          dense
+          shrink
+          class="text-cyan-13"
+          active-color="light-green"
+        >
+          <q-route-tab
+            icon="home"
+            to="/featured"
+            :label="$q.screen.gt.sm ? 'Featured' : ''"
+            exact
+          />
           <q-route-tab
             icon="folder"
             to="/portfolio"
             :label="$q.screen.gt.sm ? 'Portfolio' : ''"
             exact
           />
-          <q-route-tab icon="info" to="/about" :label="$q.screen.gt.sm ? 'About' : ''" exact />
-          <q-route-tab icon="article" to="/blog" :label="$q.screen.gt.sm ? 'Blog' : ''" exact />
+          <q-route-tab
+            icon="info"
+            to="/about"
+            :label="$q.screen.gt.sm ? 'About' : ''"
+            exact
+          />
+          <q-route-tab
+            icon="work"
+            to="/cv"
+            :label="$q.screen.gt.sm ? 'CV' : ''"
+            exact
+          />
+          <q-route-tab
+            icon="article"
+            to="/blog"
+            :label="$q.screen.gt.sm ? 'Blog' : ''"
+            exact
+          />
         </q-tabs>
       </template>
     </q-banner>
@@ -33,11 +64,11 @@
 
 <script>
 export default {
-  name: 'HeaderBar',
+  name: "HeaderBar",
   setup() {
-    return {}
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <style scoped>
--- src/router/routes.js
+++ src/router/routes.js
@@ -31,7 +31,11 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/Blog.vue") }],
   },
-
+  {
+    path: "/cv",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/CV.vue") }],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
